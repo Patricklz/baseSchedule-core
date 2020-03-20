efabca7adf5c6a528d8e09716bf7fa237031a945
Patrick <patrick_luiz@msn.com>
2020-03-20 16:57:00 -0300

    jwt

---
+++ src/app/controllers/session-controller.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+import User from '../models/user';
+import AuthConfig from '../../config/auth-config';
+
+class SessionController {
+    async store(req, res) {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            return res.status(401).json({ error: 'usuário não encontrado ' });
+        }
+
+        if (!(await user.checkPassword(password))) {
+            return res.status(401).json({ error: 'pass' });
+        }
+
+        const { id, name } = user;
+
+        return res.json({
+            user: { id, name, email },
+            token: jwt.sign({ id }, AuthConfig.authPass, {
+                expiresIn: AuthConfig.expiresIn,
+            }),
+        });
+    }
+}
+
+export default new SessionController();
---
+++ src/config/auth-config.js
@@ -0,0 +1,4 @@
+export default {
+    authPass: '0f7ab45ba505472a97ba18b83cd59e7e',
+    expiresIn: '1h',
+};
