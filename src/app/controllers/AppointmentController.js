e65a3058a2375393dfc4f593ffafe0476ab0b082
Patrick <patrick_luiz@msn.com>
2020-03-29 14:49:08 -0300

    sentry

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -16,7 +16,7 @@ class AppointmentController {
         const appointments = await Appointments.findAll({
             where: { user_id: req.userId, canceled_at: null },
             order: ['date'],
-            attributes: ['id', 'date'],
+            attributes: ['id', 'date', 'past', 'cancelable'],
             limit: 20,
             offset: (page - 1) * 20,
             include: [{ model: User, as: 'provider', attributes: ['id', 'name'], include: [{ model: File, as: 'avatar', attributes: ['id', 'path', 'url'] }] }],
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+    dsn: 'https://e5189b9887d14cc89cafba70e796fe39@sentry.io/5179372'
+};
